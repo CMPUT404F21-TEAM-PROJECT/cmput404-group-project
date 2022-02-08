e0eb9f59217f87d28245a710ba1dc6ae545e2030
Jacques857 <jleongsi@ualberta.ca>
2022-02-07 19:55:34 -0700

    added Author model

--- distributed_social_network/distributed_social_network/settings.py
+++ distributed_social_network/distributed_social_network/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'distributed_social_network',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
