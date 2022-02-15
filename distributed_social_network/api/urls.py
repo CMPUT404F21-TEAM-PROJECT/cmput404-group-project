fa292909dee0db620bd56293ecd7d4af3145442b
Jacques857 <jleongsi@ualberta.ca>
2022-02-15 12:02:30 -0700

    fixed format of author update to match spec, removed ability to update the primary key of author

--- distributed_social_network/api/urls.py
+++ distributed_social_network/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('authors/', views.route_multiple_authors, name='Add Author'),
-  path('authors/<str:id>/', views.route_single_author, name='Change Author')
+  path('authors/', views.route_multiple_authors, name='Multiple Authors'),
+  path('authors/<str:id>/', views.route_single_author, name='Single Author')
 ]
