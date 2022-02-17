663d12d8ba678370b6e7093f0a9ed32acde20f9f
Michelle <phiangda@ualberta.ca>
2022-02-17 01:15:58 -0700

    implemented follower endpoints

--- distributed_social_network/api/urls.py
+++ distributed_social_network/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
   path('authors/', views.route_multiple_authors, name='Add Author'),
-  path('authors/<str:id>/', views.route_single_author, name='Change Author')
+  path('authors/<str:id>/', views.route_single_author, name='Change Author'),
+  path('authors/<str:id>/followers/', views.route_multiple_followers, name='Get Followers'),
+  path('authors/<str:id>/followers/<str:f_id>', views.route_single_follower, name='Manage Follower'),
 ]
