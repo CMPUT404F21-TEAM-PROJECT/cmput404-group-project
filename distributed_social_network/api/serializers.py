d1d5992aafc6439e327b778b5e9f879dff404098
Josh Patrick <jjpatric@ualberta.ca>
2022-02-18 11:12:12 -0700

    Add post endpoints, update post model

--- distributed_social_network/api/serializers.py
+++ distributed_social_network/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Author
+from .models import Author, Post
 
 class AuthorSerializer(serializers.ModelSerializer):
     type = serializers.CharField(read_only = True, default = 'author')
     class Meta:
         model = Author
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(read_only = True, default = 'post')
+    class Meta:
+        model = Post
         fields = '__all__'
--- distributed_social_network/api/urls.py
+++ distributed_social_network/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('authors/', views.route_multiple_authors, name='Add Author'),
   path('authors/<str:id>/', views.route_single_author, name='Change Author')
+  path('authors/<str:id>/posts/', views.route_multiple_posts, name='Multiple Posts')
+  path('authors/<str:id>/posts/<str:id>/', views.route_single_post, name='Single Post')
 ]
