a5bcdaf8095258916c6205a820f991cd733d8bc5
Josh Patrick <jjpatric@ualberta.ca>
2022-02-18 14:36:05 -0700

    Add working post and simple unit tests

--- distributed_social_network/api/urls.py
+++ distributed_social_network/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   path('authors/', views.route_multiple_authors, name='Multiple Authors'),
-  path('authors/<str:id>/', views.route_single_author, name='Single Author')
-  path('authors/<str:id>/posts/', views.route_multiple_posts, name='Multiple Posts')
-  path('authors/<str:id>/posts/<str:id>/', views.route_single_post, name='Single Post')
+  path('authors/<str:author_id>/', views.route_single_author, name='Single Author'),
+  path('authors/<str:author_id>/posts/', views.route_multiple_posts, name='Multiple Posts'),
+  path('authors/<str:author_id>/posts/<str:post_id>/', views.route_single_post, name='Single Post'),
 ]
--- distributed_social_network/api/views.py
+++ distributed_social_network/api/views.py
@@ -8,13 +8,13 @@ from .serializers import AuthorSerializer, PostSerializer
 
 # Routes the request for a single author
 @api_view(['DELETE', 'POST', 'GET'])
-def route_single_author(request, id):
+def route_single_author(request, author_id):
     if request.method == 'DELETE':
-        return delete_author(request, id)
+        return delete_author(request, author_id)
     elif request.method == 'POST':
-        return update_author(request, id)
+        return update_author(request, author_id)
     elif request.method == 'GET':
-        return get_single_author(request, id)
+        return get_single_author(request, author_id)
 
 # Routes the request for multiple authors
 @api_view(['POST', 'GET'])
@@ -135,19 +135,19 @@ def find_author(id):
 
 # Routes the request for a single post
 @api_view(['GET', 'POST', 'DELETE', 'PUT'])
-def route_single_post(request, id):
+def route_single_post(request, author_id, post_id):
     if request.method == 'GET':
-        return get_post(request, id)
+        return get_post(request, post_id)
     elif request.method == 'POST':
-        return update_post(request, id)
+        return update_post(request, post_id)
     elif request.method == 'DELETE':
-        return delete_post(request, id)
+        return delete_post(request, post_id)
     elif request.method == 'PUT':
-        return create_post(request, id)
+        return create_post(request, post_id)
 
 # Routes the request for multiple posts
 @api_view(['POST', 'GET'])
-def route_multiple_posts(request):
+def route_multiple_posts(request, author_id):
     if request.method == 'POST':
         return create_post(request)
     elif request.method == 'GET':
