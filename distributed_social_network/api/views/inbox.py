55812124843f570fc6b704ed483c93a35dee7596
Michelle <phiangda@ualberta.ca>
2022-02-22 17:40:48 -0700

    done post requests for follow requests

--- distributed_social_network/api/views/inbox.py
+++ distributed_social_network/api/views/inbox.py
@@ -1,8 +1,8 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse, HttpResponse
 import jwt
-from ..models import Inbox
-from ..serializers import InboxSerializer
+from ..models import Inbox, Author
+from ..serializers import InboxSerializer, FollowRequestSerializer, PostSerializer
 
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
@@ -12,13 +12,31 @@ from rest_framework.pagination import PageNumberPagination
 def route_inbox(request, author_id):
     if request.method == 'GET':
         return get_inbox(request, author_id)
+
     elif request.method == 'POST':
-        pass
+        response = HttpResponse()
+        try:
+            inbox = Inbox.objects.get(author=author_id)
+        except ObjectDoesNotExist:
+            response.status_code = 404
+            return response
+
+        object_type = request.data['type']
+        if object_type == 'post':
+            pass
+        elif object_type == 'like':
+            pass
+        elif object_type == 'follow':
+            add_follow(request, author_id)
+        else:
+            response.status_code = 400
+            return response
+
     elif request.method == 'DELETE':
         pass
 
 # Get author_id's inbox
-def get_inbox(author_id):
+def get_inbox(request, author_id):
     response = HttpResponse()
     try:
         inbox = Inbox.objects.get(author=author_id)
@@ -64,4 +82,40 @@ def get_inbox(author_id):
 
     response = JsonResponse(data)
     response.status_code = 200
+    return response
+
+
+# Create a follow request and add it to author_id's inbox
+def add_follow(request, author_id):
+    try:
+        inbox = Inbox.objects.get(author=author_id)
+    except ObjectDoesNotExist:
+        response.status_code = 404
+        return response
+
+    # get the viewer (person who sent the follow request)
+    try:
+        cookie = request.COOKIES['jwt']
+        viewerId = jwt.decode(cookie, key='secret', algorithms=['HS256'])["id"]
+    except KeyError:
+        response.status_code = 401
+        return response
+    
+    # create the follow request
+    data = request.data.copy()
+    data['actor'] = viewerId
+    data['object'] = author_id
+    serializer = FollowRequestSerializer(data = data)
+    response = HttpResponse()
+
+    # If given data is valid, save the follow request to the database
+    if serializer.is_valid():
+        fr = serializer.save()
+        # add the follow request to author_id's inbox
+        inbox.follow_requests.add(fr)
+        response.status_code = 201
+        return response
+
+    print(serializer.errors)
+    response.status_code = 400
     return response
