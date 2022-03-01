7b12e839d8987d8748f9fd5f73a2501e212f854c
Victor Lieu <victorlieu@Victors-MacBook-Air.local>
2022-03-01 13:10:33 -0700

    Basic Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,44 @@
 import React, { useEffect } from "react";
-import logo from './logo.svg';
 import './App.css';
 import requests from "./requests";
+import Login from "./components/LoginRegister/login";
 
 function App() {
 
-  const fetchAuthors = async () => {
-    // sends a GET request to http://${BACKEND_URL}:${BACKEND_PORT}/service/authors
-    const response = await requests.get(`service/authors/`, {
-      // uncommenting below will send request to /authors?page=2&size=2
-      // params: {
-      //   page: 2,
-      //   size: 2
-      // },
-    });
+  // const fetchAuthors = async () => {
+  //   // sends a GET request to http://${BACKEND_URL}:${BACKEND_PORT}/service/authors
+  //   const response = await requests.get(`service/authors/`, {
+  //     // uncommenting below will send request to /authors?page=2&size=2
+  //     // params: {
+  //     //   page: 2,
+  //     //   size: 2
+  //     // },
+  //   });
 
-    console.log(response)
-    console.log('hi from fetchAuthors')
-  };
+  //   console.log(response)
+  //   console.log('hi from fetchAuthors')
+  // };
 
-  // Send the request once on loadup
-  useEffect(() => {
-    fetchAuthors();
-  }, []);
+  // // Send the request once on loadup
+  // useEffect(() => {
+  //   fetchAuthors();
+  // }, []);
   
   
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>
+  //       Login
+  //     </h1>
+  //   </div>
+  // );
+
+    return (
+      <div className="App">
+        <Login/>
+      </div>
+    )
 }
 
 export default App;
