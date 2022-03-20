b63486d0627b88415608f157b09665e6d10ed808
Victor Lieu <victor1@ualberta.ca>
2022-03-19 19:17:25 -0600

    Deployment and CICD

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import { BACKEND_PORT, BACKEND_URL } from "./constants";
 
+// UNCOMMENT THIS URL FOR DEVELOPMENT ON LOCAL
+// const url = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : `http://${BACKEND_URL}:${BACKEND_PORT}/`;
+
+// FOR PRODUCTION
+const url = "https://deployservertest.herokuapp.com/";
+
 export default axios.create({
-  baseURL: `http://${BACKEND_URL}:${BACKEND_PORT}/`,
+  baseURL: url,
   headers: {
     Authorization: localStorage.getItem('token')
     ? 'JWT ' + localStorage.getItem('access_token') : null,
