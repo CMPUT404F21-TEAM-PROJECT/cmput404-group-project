6e5509d9939dd08764a810c75ea2c2153292257d
Michelle <phiangda@ualberta.ca>
2022-03-23 14:33:16 -0600

    removed unnecessary url var and switch to production backend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 // FOR PRODUCTION
-// export const BACKEND_URL = "http://deployservertest.herokuapp.com";
+export const BACKEND_URL = "http://deployservertest.herokuapp.com";
 
 // UNCOMMENT FOR LOCAL DEVELOPMENT
-export const BACKEND_URL = "http://127.0.0.1:8000";
+// export const BACKEND_URL = "http://127.0.0.1:8000";
--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 import { BACKEND_URL } from "./constants";
 
-// UNCOMMENT THIS URL FOR DEVELOPMENT ON LOCAL
-const url = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : `${BACKEND_URL}/`;
-
-// FOR PRODUCTION
-// const url = "";
-
 export default axios.create({
   baseURL: "",
   headers: {
