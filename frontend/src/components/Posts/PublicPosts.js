e3820c0fe2a7274311d25c52309c1d4d2a84cfee
RyanHelgoth <78457606+RyanHelgoth@users.noreply.github.com>
2022-03-24 16:02:29 -0600

    PublicPosts.js urls fixed

--- frontend/src/components/Posts/PublicPosts.js
+++ frontend/src/components/Posts/PublicPosts.js
@@ -29,11 +29,9 @@ class PublicPosts extends React.Component {
         // get list of likes for each post
         const postPromises = response.data.items.map(async (item) => {
           if (item.type === 'post') {
-            const url = `${item.author.id}/posts/${item.id}/likes/`
+            const url = `${item.id}/likes/`
             const like_response = await requests.get(url);
-
-            //const like_response = await requests.get(`${item.id}/likes/`);
-
+            
             item.likes = like_response.data.items;
             item.likedByCurrent = false;
             // check if current viewer liked the post
