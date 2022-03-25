66d1cd1993ac3ee9f2bdb3dad2990c207d54b5e2
Jacques857 <jleongsi@ualberta.ca>
2022-03-25 16:58:22 -0600

    added backend getauth method

---
+++ distributed_social_network/api/util.py
@@ -0,0 +1,28 @@
+from urllib.parse import urlparse
+import environ
+import base64
+
+env = environ.Env()
+environ.Env.read_env()
+
+def getAuthHeaderForNode(id):
+    # this function extracts the node origin from the url
+    # it then returns the auth_header required for that node 
+
+    # dictionary to hold username:password for remote nodes
+    nodeAuthDict = {
+        'tik-tak-toe-cmput404.herokuapp.com':'admin:tX7^iS8a5Ky$^S',
+        'cmput-404-w22-group-10-backend.herokuapp.com':'Team4:abcd1234',
+    }
+
+    # extract node from url
+    url_obj = urlparse(id)
+    auth_header = {}
+    encodedAuth = base64.b64encode(nodeAuthDict[url_obj.hostname].encode('ascii'))
+
+    auth_header = {"headers": {
+        "Authorization": "Basic " + str(encodedAuth.decode('ascii')),
+        "accept": 'application/json',
+    }}
+
+    return auth_header
