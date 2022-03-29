bd4b5c87dece3e5564cd0e40dea4bbebe02d21bb
Jacques857 <73079488+Jacques857@users.noreply.github.com>
2022-03-29 09:47:21 -0600

    Update 0009_node_baseurl_alter_author_profileimage.py

--- distributed_social_network/api/migrations/0009_node_baseurl_alter_author_profileimage.py
+++ distributed_social_network/api/migrations/0009_node_baseurl_alter_author_profileimage.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0008_alter_author_profileimage.py'),
+        ('api', '0008_alter_author_profileimage'),
     ]
 
     operations = [
